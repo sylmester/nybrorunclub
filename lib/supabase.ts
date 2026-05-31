6dbb22d230e3d1daf281edd016beb2f5e347fe72
Sylvester <90195024+sylmester@users.noreply.github.com>
2026-05-31 23:03:28 +0200

    Dont cache supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,4 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    global: {
+      fetch: (url, options) => fetch(url, { ...options, cache: "no-store" }),
+    },
+  },
 );
--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -3,4 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  {
+    global: {
+      fetch: (url, options) => fetch(url, { ...options, cache: "no-store" }),
+    },
+  },
 );
